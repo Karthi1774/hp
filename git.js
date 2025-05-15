199e97383014c503708fa3013a3baa739dda0af9
Karthi1774 <kartee167@gmail.com>
2025-05-15 14:59:20 +0530

    use

--- git.js
+++ git.js
@@ -1,6 +1,3 @@
 console.log("hello sir im completed say one task");
-<<<<<<< HEAD
 console.log("hello new team welcome to the company");
-=======
 console.log("welcome to the team");
->>>>>>> c6ed5a1e6a638149a8214366a42a3df286f02ec5
